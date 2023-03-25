98c460c039eb79bfe04d50239a7d97052b660ee1
Saptarshi Dey <saptarshid795.tal@gmail.com>
2023-03-25 18:41:07 +0530

    Made some minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import pain from "./samurai.jpg"
+import face from "./Face.png"
 
 export class About extends Component {
     render() {
@@ -7,7 +7,7 @@ export class About extends Component {
         const style2 = { color: "white", fontSize: "1.5em", textAlign: "justify", width: "fit-content", margin: "auto", paddingLeft: "20px", paddingRight: "20px" };
         return (
             <>
-                <img src={pain} style={style1}></img><br />
+                <img src={face} style={style1}></img><br />
                 <div style={style2}><b>Saptarshi Dey</b></div><br /><br />
                 <div style={style2}>
                     I am Saptarshi Dey, currently pursuing B.Tech in Computer Science and Engineering (CSE). Checkout my <a href='https://linktr.ee/darkmortal' target='_blank'>Linktree</a>
